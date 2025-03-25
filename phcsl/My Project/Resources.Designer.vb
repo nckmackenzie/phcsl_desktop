349b846f29806a7caeb6d927634888ae85677f10
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-03-25 12:52:49 +0300

    style: sale icon addition

--- phcsl/My Project/Resources.Designer.vb
+++ phcsl/My Project/Resources.Designer.vb
@@ -213,6 +213,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property icons8_sale_16() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("icons8_sale_16", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property icons8_user_16() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("icons8_user_16", resourceCulture)
