f88af5c898b0ef745b36c18b5964b72af36c01a4
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-03-27 11:06:32 +0300

    feat: add icon for expenses

--- phcsl/My Project/Resources.Designer.vb
+++ phcsl/My Project/Resources.Designer.vb
@@ -73,6 +73,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property icons8_budget_16() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("icons8_budget_16", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property icons8_cash_and_credit_card_16() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("icons8_cash_and_credit_card_16", resourceCulture)
