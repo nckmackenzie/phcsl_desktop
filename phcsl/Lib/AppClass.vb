04d75e1e14ff8891fb03c7830ddbc98a1cd6d957
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-05-14 16:51:50 +0300

    feat: add util function to convert number to words

--- phcsl/Lib/AppClass.vb
+++ phcsl/Lib/AppClass.vb
@@ -619,4 +619,44 @@ Public Class AppClass
     Public Shared Function IsValidDateRange(fromDate As Date, toDate As Date) As Boolean
         Return fromDate.Date < toDate.Date
     End Function
+    Public Shared Function NumberToText(ByVal n As Integer) As String
+        Select Case n
+            Case 0
+                Return ""
+
+            Case 1 To 19
+                Dim arr() As String = {"One", "Two", "Three", "Four", "Five", "Six", "Seven",
+                  "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen",
+                    "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"}
+                Return arr(n - 1) & " "
+
+            Case 20 To 99
+                Dim arr() As String = {"Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"}
+                Return arr(n \ 10 - 2) & " " & NumberToText(n Mod 10)
+
+            Case 100 To 199
+                Return "One Hundred " & NumberToText(n Mod 100)
+
+            Case 200 To 999
+                Return NumberToText(n \ 100) & "Hundreds " & NumberToText(n Mod 100)
+
+            Case 1000 To 1999
+                Return "One Thousand " & NumberToText(n Mod 1000)
+
+            Case 2000 To 999999
+                Return NumberToText(n \ 1000) & "Thousands " & NumberToText(n Mod 1000)
+
+            Case 1000000 To 1999999
+                Return "One Million " & NumberToText(n Mod 1000000)
+
+            Case 1000000 To 999999999
+                Return NumberToText(n \ 1000000) & "Millions " & NumberToText(n Mod 1000000)
+            Case 1000000000 To 1999999999
+                Return "One Billion " & NumberToText(n Mod 1000000000)
+
+            Case Else
+                Return NumberToText(n \ 1000000000) & "Billion " _
+                  & NumberToText(n Mod 1000000000)
+        End Select
+    End Function
 End Class
